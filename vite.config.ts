878c3bd97d04c69eaa8052d54fc32b2236803cd5
Sangeeta <sangeetaswainlucky@gmail.com>
2025-04-04 19:16:54 +0530

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,14 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 import { defineConfig } from "vite";
 import wasm from "vite-plugin-wasm";
 
 export default defineConfig({
   plugins: [react(), tailwindcss(), wasm()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src")
-    }
-  },
-  build: {
-    assetsDir: "assets",
-    assetsInlineLimit: 51200,
-    rollupOptions: {
-      output: {
-        assetFileNames: "assets/[name][extname]"
-      }
+  base: "/",
+  server: {
+    fs: {
+      allow: ["."]
     }
   }
 });
